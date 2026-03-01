6b47ef6f8aeed219b53df137c44da478d5508e1b
Ayesha Sarwarr <ayeshasarwar422@gmail.com>
2026-03-02 01:11:15 +0500

    modify some files

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,23 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      return callback(new Error("CORS not allowed"), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
+// app.use(cors())
 app.use(express.json())
 
 const PerfumesRoutes = require("./routes/PerfumesRoutes")
